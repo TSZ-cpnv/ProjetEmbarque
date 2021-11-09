3a20b924a7a9052430efe14e5154c745c7ffd3c9
TSZ-cpnv <79205359+TSZ-cpnv@users.noreply.github.com>
2021-11-09 21:57:14 +0100

    Version 2.0 sans commentaire

--- ProjetEmbarque/Musique.h
+++ ProjetEmbarque/Musique.h
@@ -1,252 +1,237 @@
 int tonePin = 6;
+int led[]={2,3,4,5};
 
+int randomLed;
+int nombreRandom;
+
+
+void allumerEteindreLed(){
+    digitalWrite(led[randomLed],LOW);
+    do
+    {
+        nombreRandom = random(0,4); 
+    } while (nombreRandom==randomLed);
+    randomLed=nombreRandom; 
+    digitalWrite(led[randomLed],HIGH);      
+}
+
+void allumerCoter(){
+    digitalWrite(led[0],HIGH);
+    digitalWrite(led[3],HIGH);     
+    digitalWrite(led[1],LOW);
+    digitalWrite(led[2],LOW);   
+}
+void allumerCentre(){
+    digitalWrite(led[0],LOW);
+    digitalWrite(led[3],LOW);     
+    digitalWrite(led[1],HIGH);
+    digitalWrite(led[2],HIGH);   
+}
+void allumerTout(){
+      for (size_t i = 0; i < 4; i++)
+      {
+        digitalWrite(led[i],HIGH);
+      }   
+}
+void eteindreTout(){
+    for (size_t i = 0; i < 4; i++)
+      {
+        digitalWrite(led[i],LOW);
+      }  
+}
 
 void musiqueIntro() 
 {
-    tone(tonePin, 391, 118.990575);
-    delay(120.1925);
-    delay(120.1925);
-    tone(tonePin, 391, 118.990575);
-    delay(120.1925);
-    delay(120.1925);
-    tone(tonePin, 391, 118.990575);
-    delay(120.1925);
-    delay(120.1925);
-    tone(tonePin, 391, 237.98115);
-    delay(240.385);
-    tone(tonePin, 415, 118.990575);
-    delay(120.1925);
-    tone(tonePin, 391, 118.990575);
-    delay(120.1925);
-    tone(tonePin, 349, 118.990575);
-    delay(120.1925);
-    tone(tonePin, 391, 118.990575);
-    delay(120.1925);
-    delay(120.1925);
-    tone(tonePin, 349, 118.990575);
-    delay(120.1925);
-    delay(120.1925);
-    tone(tonePin, 311, 237.98115);
-    delay(240.385);
-    delay(120.1925);
-    tone(tonePin, 311, 118.990575);
-    delay(120.1925);
-    delay(120.1925);
-    tone(tonePin, 311, 118.990575);
-    delay(120.1925);
-    delay(120.1925);
-    tone(tonePin, 311, 237.98115);
-    delay(240.385);
-    tone(tonePin, 293, 237.98115);
-    delay(240.385);
-    tone(tonePin, 311, 237.98115);
-    delay(240.385);
-    tone(tonePin, 349, 237.98115);
-    delay(240.385);
-    tone(tonePin, 391, 475.9623);
-    delay(480.77);
-    tone(tonePin, 311, 356.971725);
-    delay(360.5775);
-    delay(120.1925);
-    tone(tonePin, 311, 237.98115);
-    delay(240.385);
-    tone(tonePin, 293, 237.98115);
-    delay(240.385);
-    tone(tonePin, 311, 237.98115);
-    delay(240.385);
-    tone(tonePin, 349, 237.98115);
-    delay(240.385);
-    tone(tonePin, 391, 475.9623);
-    delay(480.77);
-    tone(tonePin, 391, 475.9623);
-    delay(480.77);
-    tone(tonePin, 311, 237.98115);
-    delay(240.385);
-    tone(tonePin, 391, 475.9623);
-    delay(480.77);
-    tone(tonePin, 349, 475.9623);
-    delay(480.77);
-    tone(tonePin, 311, 594.952875);
-    delay(600.9625);
-}
-
-void musiquePerdu(){
-    tone(tonePin, 220, 37.5);
-    delay(37.5);
-    tone(tonePin, 246, 56.25);
-    delay(56.25);
-    tone(tonePin, 329, 56.25);
-    delay(56.25);
-    tone(tonePin, 369, 1031.25);
-    delay(1031.25);
-    delay(18.75);
-    tone(tonePin, 554, 375.0);
-    delay(375.0);
-    tone(tonePin, 659, 1275.0);
-    delay(1275.0);
-    delay(56.25);
-    tone(tonePin, 220, 56.25);
-    delay(56.25);
-    tone(tonePin, 246, 56.25);
-    delay(56.25);
-    tone(tonePin, 329, 56.25);
-    delay(56.25);
-    tone(tonePin, 369, 1031.25);
-    delay(1031.25);
-    delay(18.75);
-    tone(tonePin, 554, 375.0);
-    delay(375.0);
-    tone(tonePin, 659, 1275.0);
-    delay(1275.0);
-    delay(56.25);
-    tone(tonePin, 207, 56.25);
-    delay(56.25);
-    tone(tonePin, 246, 56.25);
-    delay(56.25);
-    tone(tonePin, 329, 56.25);
-    delay(56.25);
-    tone(tonePin, 369, 1031.25);
-    delay(1031.25);
-    delay(18.75);
-    tone(tonePin, 659, 600.0);
-    delay(600.0);
-    tone(tonePin, 739, 600.0);
-    delay(600.0);
-    tone(tonePin, 329, 56.25);
-    delay(56.25);
-    tone(tonePin, 440, 56.25);
-    delay(56.25);
-    tone(tonePin, 493, 56.25);
-    delay(56.25);
-    tone(tonePin, 554, 1031.25);
-    delay(1031.25);
-    tone(tonePin, 554, 300.0);
-    delay(300.0);
-    tone(tonePin, 493, 300.0);
-    delay(300.0);
-    tone(tonePin, 440, 300.0);
-    delay(300.0);
-    tone(tonePin, 415, 300.0);
-    delay(300.0);
-    tone(tonePin, 369, 750.0);
-    delay(750.0);
+    tone(tonePin, 246, 164.835);
+    allumerEteindreLed();
+    delay(164.835);
+    tone(tonePin, 220, 164.835);
+    allumerEteindreLed();
+    delay(164.835);
+    tone(tonePin, 246, 164.835);
+    allumerEteindreLed();
+    delay(164.835);
+    tone(tonePin, 293, 329.67);
+    allumerEteindreLed();
+    delay(329.67);
+    tone(tonePin, 220, 164.835);
+    allumerEteindreLed();
+    delay(164.835);
+    tone(tonePin, 246, 164.835);
+    allumerEteindreLed();
+    delay(164.835);
+    tone(tonePin, 220, 164.835);
+    allumerEteindreLed();
+    delay(164.835);
+    tone(tonePin, 246, 164.835);
+    allumerEteindreLed();
+    delay(164.835);
+    tone(tonePin, 293, 164.835);
+    allumerEteindreLed();
+    delay(164.835);
+    tone(tonePin, 246, 164.835);
+    allumerCoter();
+    delay(164.835);
+    tone(tonePin, 220, 164.835);
+    allumerCentre();
+    delay(164.835);
+    tone(tonePin, 246, 329.67);
+    allumerTout();
+    delay(329.67);
+    eteindreTout();
 }
 
 void musiqueGagner(){
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 293, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 440, 797.447727273);
+    allumerEteindreLed();
     delay(797.447727273);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 293, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 523, 318.979090909);
+    allumerEteindreLed();
     delay(318.979090909);
     tone(tonePin, 493, 318.979090909);
+    allumerEteindreLed();
     delay(318.979090909);
     tone(tonePin, 440, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 293, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 440, 797.447727273);
+    allumerEteindreLed();
     delay(797.447727273);
     tone(tonePin, 349, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 261, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 349, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 466, 318.979090909);
+    allumerEteindreLed();
     delay(318.979090909);
     tone(tonePin, 440, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 293, 79.7447727273);
+    allumerEteindreLed();
     delay(79.7447727273);
     tone(tonePin, 329, 79.7447727273);
+    allumerEteindreLed();
     delay(79.7447727273);
     tone(tonePin, 349, 79.7447727273);
+    allumerEteindreLed();
     delay(79.7447727273);
     tone(tonePin, 369, 79.7447727273);
+    allumerEteindreLed();
     delay(79.7447727273);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 293, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 440, 797.447727273);
+    allumerEteindreLed();
     delay(797.447727273);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 293, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 523, 318.979090909);
+    allumerEteindreLed();
     delay(318.979090909);
     tone(tonePin, 493, 318.979090909);
+    allumerEteindreLed();
     delay(318.979090909);
     tone(tonePin, 440, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 293, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 391, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 440, 637.958181818);
+    allumerEteindreLed();
     delay(637.958181818);
     tone(tonePin, 349, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 261, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 349, 159.489545455);
+    allumerEteindreLed();
     delay(159.489545455);
     tone(tonePin, 466, 318.979090909);
+    allumerCentre();
     delay(318.979090909);
     tone(tonePin, 440, 318.979090909);
+    allumerCoter();
     delay(318.979090909);
     tone(tonePin, 349, 318.979090909);
+    allumerTout();
     delay(318.979090909);
-}
-
-void faux(){
-    tone(tonePin, 250, 200);
-    delay(250);
-    tone(tonePin, 250, 200);
-}
-
-void juste(){
-    tone(tonePin, 350, 200);
-    delay(250);
-    tone(tonePin, 350, 200);
-
+    eteindreTout();
 }
 
 void noteBleu(){
-    tone(tonePin,329, 250);
-    delay(250);
+    tone(tonePin,329, 200);
+    delay(200);
 }
 void noteJaune(){
-    tone(tonePin,261, 250);
-    delay(250);
+    tone(tonePin,261, 200);
+    delay(200);
 }
 void noteRouge(){
-    tone(tonePin,220, 250);
-    delay(250);
+    tone(tonePin,220, 200);
+    delay(200);
 }
 void noteVert(){
-    tone(tonePin,163, 250);
-    delay(250);
+    tone(tonePin,163, 200);
+    
+    delay(200);
+}
+void notePerdu(){
+    tone(tonePin,33, 500);
+    delay(1000);
 }
--- ProjetEmbarque/ProjetEmbarque.ino
+++ ProjetEmbarque/ProjetEmbarque.ino
@@ -1,12 +1,18 @@
+/*
+* Auteur : Thomas Petermann
+* Version : 1.0
+* Date : 09.11.2021
+* description : Ce programme permet de jouer au "Simon says"
+*/
+
+
 #include "Musique.h"
 
 int frequenceNote[]={329,261,220,163};
-int debounce[]={0,0,0,0};
+int debounce=0;
 int sequence[]={};
-int led[]={2,3,4,5};
-int compteur=1;
-
-
+int longueurSequence=1;
+int vitesse=500;
 
 int boutonBleu=0;
 int boutonJaune=0;
@@ -18,7 +24,7 @@ int perdu=0;
 
 
 void setup() {
-  randomSeed(analogRead(0));
+
   for(int i = 2; i < 6; i++)
   {
     pinMode(i, OUTPUT);
@@ -28,128 +34,176 @@ void setup() {
   pinMode(A3, INPUT_PULLUP);
   pinMode(A4, INPUT_PULLUP);
   pinMode(A5, INPUT_PULLUP);
-  
-  
-  sequence[compteur-1]= random(2,6);
-  
 
-  
 }
 
-void loop(){
+void loop()
+{
+
   if (digitalRead(A2)==LOW)
   {
-    musiqueIntro();
-    
-  do{
-  //affiche la sequence
-  delay(500);
-  for (int i = 0; i < compteur; i++) {
-    delay(250);
-    digitalWrite(sequence[i], HIGH);
-    tone(tonePin, frequenceNote[(sequence[i]-2)], 500);
-    delay(500);
-    digitalWrite(sequence[i], LOW);    
-    }
-    
-   do{
-      // Lis si un bouton est appuier
-      boutonBleu=digitalRead(A2);
-      boutonJaune=digitalRead(A3);
-      boutonRouge=digitalRead(A4);
-      boutonVert=digitalRead(A5);
+    premierModeDeJeu();
+  }    
 
-      // Regarde si le bouton appuier est le bon
-      //Bouton bleu
-      if(boutonBleu==LOW && led[0]==sequence[bonneReponse] && debounce[0]==0)
+}
+
+void premierModeDeJeu(){
+
+    musiqueIntro();
+    choisisPremierLedAleatoire();
+
+    do{
+
+      delay(500);
+      afficheSequence();  
+      verifieReponse();
+      augmenteLaVitesse();
+      verifieResultat();  
+
+    } while ((longueurSequence-1)!=15);
+
+    delay(500);      
+    musiqueGagner(); 
+
+}
+
+void verifieResultat(){
+
+  if(bonneReponse==longueurSequence){   
+    rajouteUneLedRandom();
+  }     
+  if(perdu==1){
+    reinitialiserJeu();   
+  }
+
+}
+
+void choisisPremierLedAleatoire(){
+
+  longueurSequence=1;
+  randomSeed(analogRead(0));
+  sequence[longueurSequence-1]= random(2,6); 
+
+}
+
+void rajouteUneLedRandom(){
+
+  randomSeed(analogRead(0));
+  longueurSequence++;
+  bonneReponse=0;
+  sequence[longueurSequence-1]= random(2,6); 
+
+}
+
+void reinitialiserJeu(){
+
+  notePerdu();  
+  musiqueIntro();  
+  longueurSequence=1;
+  bonneReponse=0;
+  perdu=0; 
+
+}
+
+void augmenteLaVitesse(){
+
+  if (longueurSequence%3==0)
+  {
+    vitesse=vitesse*0.9;
+  } 
+
+}
+
+void afficheSequence(){
+
+  for (int i = 0; i < longueurSequence ; i++) {
+    delay(vitesse);
+    digitalWrite(sequence[i], HIGH);
+    tone(tonePin, frequenceNote[(sequence[i]-2)], vitesse);
+    delay(vitesse);
+    digitalWrite(sequence[i], LOW);    
+  }
+
+}
+
+void verifieReponse(){
+
+  do{
+            
+    boutonBleu=digitalRead(A2);
+    boutonJaune=digitalRead(A3);
+    boutonRouge=digitalRead(A4);
+    boutonVert=digitalRead(A5);
+  
+    switch (sequence[bonneReponse])
+    {
+
+    case 2 :
+      if(boutonBleu==LOW && debounce==0)
       {     
         noteBleu();
-          bonneReponse++;          
-          debounce[0]=1;              
-        }else if(boutonBleu==LOW && led[0]!=sequence[bonneReponse] && debounce[0]==0)
-        {  
-          musiquePerdu();     
-          perdu++;         
-          debounce[0]=1;            
-        }       
-      if(boutonBleu==HIGH && debounce[0]==1){
-        debounce[0]=0;
-        }
+        bonneReponse++;          
+        debounce=1;              
+      }else if(boutonVert==LOW || boutonRouge==LOW || boutonJaune==LOW)
+      {       
+        perdu=1;                   
+      }
+      if(boutonBleu==LOW && debounce==1)
+      {              
+        debounce=0;         
+      }
+      break;
 
-      //Bouton jaune
-      if(boutonJaune==LOW && led[1]==sequence[bonneReponse] && debounce[1]==0)
-      {  
+    case 3 :
+      if(boutonJaune==LOW && debounce==0)
+      {     
         noteJaune();
-          bonneReponse++;          
-          debounce[1]=1;              
-        }else if(boutonJaune==LOW && led[1]!=sequence[bonneReponse] && debounce[1]==0)
-        { 
-          musiquePerdu(); 
-          perdu++;         
-          debounce[1]=1;            
-        }       
-      if(boutonJaune==HIGH && debounce[1]==1){
-        debounce[1]=0;
-        }  
+        bonneReponse++;          
+        debounce=1;               
+      }else if(boutonVert==LOW || boutonRouge==LOW || boutonBleu==LOW)
+      {     
+        perdu=1;                     
+      }
+      if(boutonJaune==LOW && debounce==1)
+      {              
+        debounce=0;         
+      }
+      break;  
 
-      //Bouton rouge
-      if(boutonRouge==LOW && led[2]==sequence[bonneReponse] && debounce[2]==0)
-      {  
+    case 4 :
+      if(boutonRouge==LOW && debounce==0)
+      {     
         noteRouge();
-          bonneReponse++;          
-          debounce[2]=1;              
-        }else if(boutonRouge==LOW && led[2]!=sequence[bonneReponse] && debounce[2]==0)
-        { 
-          musiquePerdu(); 
-          perdu++;         
-          debounce[2]=1;            
-        }       
-      if(boutonRouge==HIGH && debounce[2]==1){
-        debounce[2]=0;
-        } 
+        bonneReponse++;          
+        debounce=1;               
+      }else if(boutonVert==LOW || boutonBleu==LOW || boutonJaune==LOW)
+      {       
+        perdu=1;                  
+      }
+      if(boutonRouge==LOW && debounce==1)
+      {              
+        debounce=0;         
+      }
+      break;
 
-      //Bouton vert
-      if(boutonVert==LOW && led[3]==sequence[bonneReponse] && debounce[3]==0)
-      {  
+    case 5 :
+      if(boutonVert==LOW && debounce==0)
+      {     
         noteVert();
-          bonneReponse++;          
-          debounce[3]=1;              
-        }else if(boutonVert==LOW && led[3]!=sequence[bonneReponse] && debounce[3]==0)
-        { 
-         musiquePerdu(); 
-          perdu++;         
-          debounce[3]=1;            
-        }       
-      if(boutonVert==HIGH && debounce[3]==1){
-        debounce[3]=0;
-        } 
-     
-  }while(perdu!=1 && bonneReponse!=compteur);
-
-  
-  
-    if(bonneReponse==compteur){   
-      compteur++;
-      bonneReponse=0;
-      sequence[compteur-1]= random(2,6);
-
-      
+        bonneReponse++;          
+        debounce=1;              
+      }else if(boutonBleu==LOW || boutonRouge==LOW || boutonJaune==LOW)
+      {       
+        perdu=1;                    
       }
-    
-    if(perdu==1){    
-      compteur=1;
-      bonneReponse=0;
-      perdu=0;
-      
+      if(boutonVert==LOW && debounce==1)
+      {              
+        debounce=0;         
       }
-    } while ((compteur-1)!=31);
-    delay(500);
-    musiqueGagner();
-    compteur=1;
-  }
-  
-  
-     
+      break;  
+    } 
+  }while(perdu!=1 && bonneReponse!=longueurSequence);
+
 }
 
 
